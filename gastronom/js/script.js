4c9666d88ef7ca70cb7fb4536565d3c9b3656983
mikillin <sergey.rogachevsky@gmail.com>
2019-02-01 11:02:06 +0100

    works, but very draft

--- gastronom/js/script.js
+++ gastronom/js/script.js
@@ -1,2 +1,20 @@
+$( document ).ready(function() { 
+var acc = $(".accordion");
+var i;
 
 
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = $(".subitems");
+    
+     if (panel.css("display") === "inline-block") {
+      panel.css("display","none");
+    } else {
+      panel.css("display" , "inline-block");
+    }
+
+
+
+  });
+}});
