899e1bcac4813e558a7044606243f1f679f179cc
mikillin <sergey.rogachevsky@gmail.com>
2019-02-05 17:18:43 +0100

    it seems everything works now

--- gastronom/js/script.js
+++ gastronom/js/script.js
@@ -1,21 +1,25 @@
-$( document ).ready(function() {
-var acc = $("#accordion");
-acc.bind("click", function() {
-acc.toggleClass("active");
+$(document).ready(function () {
 
-var panel = $(".subitem");
-if (panel.css("display") === "inline-block") {
-panel.css("display", "none");
-} else {
-panel.css("display", "inline-block");
-}
-});
+    var acc = $("#accordion");
+    acc.bind("click", function () {
+        acc.toggleClass("active");
+
+        var panel = $(".subitem");
+        if (panel.css("display") === "inline-block") {
+            panel.css("display", "none");
+        } else {
+            panel.css("display", "inline-block");
+        }
+
+    });
 
 
-var link = $(".link");
-link.bind("click", function() {
-	console.log($(this).attr("data-category"));
-window.location.href = $(this).attr("data-category") + ".html";
+    var link = $(".link");
+    link.bind("click", function (e) {
+        e.stopPropagation()
+        console.log($(this).attr("data-category"));
+        if (e.target.id != "accordion")
+            window.location.href = $(this).attr("data-category") + ".html";
 
-});
+    });
 });
